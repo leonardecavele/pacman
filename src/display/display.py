b86b45019888bbec67f4e43975ad26b3fda6011c
relaforg <remi.laforgue@gmail.com>
2026-03-24 16:05:10 +0100

    Better walls done

--- src/display/display.py
+++ src/display/display.py
@@ -27,20 +27,22 @@ class EnhancedCell(BaseModel):
 
 class Display:
     def __init__(self, maze: list[list[int]]):
-        self.generator = MazeGenerator()
-        self.generator.generate(42)
+        self.generator = MazeGenerator(seed=12121)
+        self.generator.generate()
         self.maze: list[list[EnhancedCell]] = self._enhanced_maze(maze)
-        print(self.maze)
-        rl.init_window(1080, 720, "Pac-Man")
+        self.width = 720
+        self.height = 720
+        rl.init_window(self.width, self.height, "Pac-Man")
         rl.set_target_fps(60)
-        self.gap = 10
+        self.gap = 18
         cols = len(self.maze[0])
         rows = len(self.maze)
         self.cell_size = min(
-            (1080 - (cols - 1) * self.gap) // cols,
-            (720 - (rows - 1) * self.gap) // rows,
+            (self.width - (cols - 1) * self.gap) // cols,
+            (self.height - (rows - 1) * self.gap) // rows,
         ) - 1
-        self.maze_image = rl.gen_image_color(1080, 720, rl.BLACK)
+        self.maze_image = rl.gen_image_color(self.width, self.height, rl.BLACK)
+        # self._DEBUG_show_grid()
         self.draw_maze()
         self.maze_texture = rl.load_texture_from_image(self.maze_image)
 
@@ -51,6 +53,14 @@ class Display:
             rl.end_drawing()
 
         rl.close_window()
+
+    def _DEBUG_show_grid(self):
+        for i in range(self.width // (self.cell_size + self.gap)):
+            x = i * (self.cell_size + self.gap) - self.gap // 2
+            rl.image_draw_line(self.maze_image, x, 0, x, self.height, rl.RED)
+        for i in range(self.height // (self.cell_size + self.gap)):
+            y = i * (self.cell_size + self.gap) - self.gap // 2
+            rl.image_draw_line(self.maze_image, 0, y, self.width, y, rl.RED)
 
     def _enhanced_maze(self, maze: list[list[int]]):
         new: list[list[EnhancedCell]] = []
@@ -102,30 +112,175 @@ class Display:
                                    y + self.cell_size,
                                    x + self.cell_size + self.gap,
                                    y + self.cell_size, rl.WHITE)
+            # right hemicircle
+            if (c_bot and c_right and not c.right and not c_right.bot
+                    and not c_bot.right):
+                rl.image_draw_circle_lines(self.maze_image,
+                                           x + self.cell_size,
+                                           y + self.cell_size + self.gap // 2,
+                                           self.gap // 2, rl.WHITE
+                                           )
+                rl.image_draw_rectangle(self.maze_image,
+                                        x + self.cell_size - self.gap,
+                                        y + self.cell_size + 1,
+                                        self.gap,
+                                        self.gap - 1, rl.BLACK)
         if (c.top):
             if (c_right and not c.right and c_right.top):
                 rl.image_draw_line(self.maze_image, x + self.cell_size,
                                    y,
                                    x + self.cell_size + self.gap,
                                    y, rl.WHITE)
+            # left hemicircle
+            if (c_top and c_left and not c.left and not c_left.top and not c_top.left):
+                rl.image_draw_circle_lines(self.maze_image,
+                                           x, y - self.gap // 2,
+                                           self.gap // 2, rl.WHITE
+                                           )
+                rl.image_draw_rectangle(self.maze_image,
+                                        x,
+                                        y - self.gap + 1,
+                                        self.gap,
+                                        self.gap - 1, rl.BLACK)
         if (c.left):
             if (c_bot and not c.bot and c_bot.left):
                 rl.image_draw_line(self.maze_image, x,
                                    y + self.cell_size,
                                    x,
                                    y + self.cell_size + self.gap, rl.WHITE)
+            # bot hemicircle
+            if (c_left and c_bot and not c.bot and not c_bot.left
+                    and not c_left.bot):
+                rl.image_draw_circle_lines(self.maze_image,
+                                           x - self.gap // 2,
+                                           y + self.cell_size,
+                                           self.gap // 2,
+                                           rl.WHITE)
+                rl.image_draw_rectangle(self.maze_image, x - self.gap + 1,
+                                        y + self.cell_size - self.gap,
+                                        self.gap - 1,
+                                        self.gap, rl.BLACK)
         if (c.right):
             if (c_bot and not c.bot and c_bot.right):
                 rl.image_draw_line(self.maze_image, x + self.cell_size,
                                    y + self.cell_size,
                                    x + self.cell_size,
                                    y + self.cell_size + self.gap, rl.WHITE)
-            if (c_right and c_top and not c.top and not c_top.right):
+            # top hemicircle
+            if (c_right and c_top and not c.top and not c_top.right
+                    and not c_right.top):
                 rl.image_draw_circle_lines(self.maze_image,
                                            x + self.cell_size
                                            + (self.gap // 2), y,
                                            self.gap // 2,
                                            rl.WHITE)
+                rl.image_draw_rectangle(self.maze_image, x + self.cell_size + 1,
+                                        y,
+                                        self.gap,
+                                        self.gap, rl.BLACK)
+
+        # bottom_right corner
+        if (c_right and c_bot and c_right.left and not c_right.bot
+                and c_bot.top and not c_bot.right):
+            rl.image_draw_circle_lines(self.maze_image,
+                                       x + self.cell_size,
+                                       y + self.cell_size,
+                                       self.gap, rl.WHITE)
+            rl.image_draw_rectangle(self.maze_image,
+                                    x + self.cell_size + 1,
+                                    y,
+                                    self.gap - 1,
+                                    self.cell_size,
+                                    rl.BLACK)
+            rl.image_draw_rectangle(self.maze_image,
+                                    x,
+                                    y + self.cell_size + 1,
+                                    self.cell_size,
+                                    self.gap - 1,
+                                    rl.BLACK)
+            rl.image_draw_rectangle(self.maze_image,
+                                    x + self.cell_size - self.gap,
+                                    y + self.cell_size - self.gap,
+                                    self.gap,
+                                    self.gap,
+                                    rl.BLACK)
+        # top_left corner
+        if (c_top and c_left and c_top.bot and not c_top.left
+                and c_left.right and not c_left.top):
+            rl.image_draw_circle_lines(self.maze_image,
+                                       x,
+                                       y,
+                                       self.gap, rl.WHITE)
+            rl.image_draw_rectangle(self.maze_image,
+                                    x,
+                                    y - self.gap + 1,
+                                    self.cell_size,
+                                    self.gap - 1,
+                                    rl.BLACK)
+            rl.image_draw_rectangle(self.maze_image,
+                                    x - self.gap + 1,
+                                    y,
+                                    self.gap - 1,
+                                    self.cell_size,
+                                    rl.BLACK)
+            rl.image_draw_rectangle(self.maze_image,
+                                    x + 1,
+                                    y + 1,
+                                    self.gap,
+                                    self.gap,
+                                    rl.BLACK)
+
+        # bottom_left corner
+        if (c_left and c_bot and c_left.right and not c_left.bot
+                and c_bot.top and not c_bot.left):
+            rl.image_draw_circle_lines(self.maze_image,
+                                       x,
+                                       y + self.cell_size,
+                                       self.gap, rl.WHITE)
+            rl.image_draw_rectangle(self.maze_image,
+                                    x,
+                                    y + self.cell_size + 1,
+                                    self.cell_size,
+                                    self.gap - 1,
+                                    rl.BLACK)
+            rl.image_draw_rectangle(self.maze_image,
+                                    x - self.gap + 1,
+                                    y,
+                                    self.gap - 1,
+                                    self.cell_size,
+                                    rl.BLACK)
+            rl.image_draw_rectangle(self.maze_image,
+                                    x + 1,
+                                    y + self.cell_size - self.gap,
+                                    self.gap,
+                                    self.gap,
+                                    rl.BLACK)
+
+        # bottom_right corner
+        if (c_top and c_right and c_top.bot and not c_top.right
+                and c_right.left and not c_right.top):
+            rl.image_draw_circle_lines(self.maze_image,
+                                       x + self.cell_size,
+                                       y,
+                                       self.gap, rl.WHITE)
+            rl.image_draw_rectangle(self.maze_image,
+                                    x,
+                                    y - self.gap + 1,
+                                    self.cell_size,
+                                    self.gap - 1,
+                                    rl.BLACK)
+            rl.image_draw_rectangle(self.maze_image,
+                                    x + self.cell_size + 1,
+                                    y,
+                                    self.gap - 1,
+                                    self.cell_size,
+                                    rl.BLACK)
+            rl.image_draw_rectangle(self.maze_image,
+                                    x + self.cell_size - self.gap,
+                                    y + 1,
+                                    self.gap,
+                                    self.gap,
+                                    rl.BLACK)
 
     def put_cell(self, c: EnhancedCell, cell_x: int, cell_y: int) -> None:
         """Draw cell wall on image
@@ -133,7 +288,11 @@ class Display:
         Args:
         cell_x, cell_y: Cell coordinates
         """
+        self.put_links(c, cell_x, cell_y)
         if (c.top and c.bot and c.left and c.right):
+            rl.image_draw_rectangle(self.maze_image, cell_x, cell_y,
+                                    self.cell_size + 1, self.cell_size + 1,
+                                    rl.BLACK)
             return
         if (c.top):
             rl.image_draw_line(self.maze_image, cell_x, cell_y, cell_x +
@@ -147,4 +306,3 @@ class Display:
         if (c.left):
             rl.image_draw_line(self.maze_image, cell_x, cell_y,
                                cell_x, cell_y + self.cell_size, rl.WHITE)
-        self.put_links(c, cell_x, cell_y)
